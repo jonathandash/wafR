40da30eebc65b76534e3ce5da53a66c6f7f08b4c
jonathandash <jonathan.dash@scionresearch.com>
2021-02-17 16:36:56 +0000

    Added MagGroome_Pal function

--- R/wafR_functions.R
+++ R/wafR_functions.R
@@ -10,7 +10,7 @@ MagGroome_colours <- c(
   `mgclgrey` = '#FAFAFA',
   `mgcmgrey` = '#C8C8C8',
   `mgcgrey` = '#969696',
-  `mgcgrey` = '#7f7f7f',
+  `mgcdgrey` = '#7f7f7f',
   `mgcyellow` = '#FAFA64',
   `mgclorange` = '#FAC800',
   `mgcorange` = '#FA7D00',
@@ -37,6 +37,37 @@ MagGroome_cols <- function(...) {
 
   return(MagGroome_colours[cols])
 }
+
+
+MagGroome_palettes <- list(
+  `main`  = MagGroome_cols("mgclgrey", "mgcmgrey",
+                           "mgcgrey", "mgcdgrey",
+                           "mgcyellow","mgclorange",
+                           "mgcorange", "mgcllgreen",
+                           "mgcgreen","mgclightgreen",
+                           "mgcblue"))
+
+
+#' Return function to interpolate a Mag Groome color palette
+#'
+#' @param palette Character name of palette in MagGroome_palettes
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments to pass to colorRampPalette()
+#' @export
+#' @examples
+#' MagGroome_pal('main') (10)
+#'
+MagGroome_pal <- function(palette = "main", reverse = FALSE, ...) {
+  pal <- MagGroome_palettes[[palette]]
+
+  if (reverse) pal <- rev(pal)
+
+  grDevices::colorRampPalette(pal, ...)
+}
+
+
+# This can now be used to interpolate colour through the Mag Groome colours and this
+# can then be used for custom ggplot scales.
 
 
 
