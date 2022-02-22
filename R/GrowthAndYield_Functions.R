bc0c202bcaa5364a87d5c0811a20884e9d315f79
jonathandash <jonathan.dash@margulesgroome.com>
2022-02-22 16:03:38 +0000

    Add in the Max and Burkhart taper function with SA handbook coefficients

--- R/GrowthAndYield_Functions.R
+++ R/GrowthAndYield_Functions.R
@@ -175,3 +175,124 @@ vol_tree_safh <- function(DBH,
   return(V)
 
 }
+
+
+#' Stem taper
+#'
+#' Max and Burkhart taper function with regression coefficients from the
+#' South African forestry handbook
+#'
+#' @param DBH Tree DBH in cm
+#' @param H Tree height in m
+#' @param h Stem height
+#' @param species Tree species
+#' @param a1 Equation coefficient
+#' @param a2 Equation coefficient
+#' @param b5 Equation coefficient
+#' @param b6 Equation coefficient
+#' @param b7 Equation coefficient
+#' @param b8 Equation coefficient
+#'
+#' @return stem diameter at height h
+#' @export
+#'
+#' @examples
+tpr_max_burkhart_safh<-function(DBH,
+                       H,
+                       h,
+                       species,
+                       a1,
+                       a2,
+                       b5,
+                       b6,
+                       b7,
+                       b8)
+
+{
+
+  if(species == "A. mearnsii")
+  {
+
+    b5 = -3.547
+    b6 = 1.683
+    b7 = -1.198
+    b8 = 55.754
+    a1 = 0.781
+    a2 = 0.121
+
+  }else if(species == "E. grandis")
+  {
+    b5 = -3.942
+    b6 = 1.910
+    b7 = -1.712
+    b8 = 125.128
+    a1 = 0.834
+    a2 = 0.048
+  }else if(species == "E. grandis x urophylla")
+  {
+    b5 = -1.796
+    b6 = 0.649
+    b7 = -0.318
+    b8 = 119.924
+    a1 = 0.819
+    a2 = 0.050
+  }
+  else if(species == "E. macarthurii")
+  {
+    b5 = -2.832
+    b6 = 1.124
+    b7 = -0.896
+    b8 = 16.228
+    a1 = 0.724
+    a2 = 0.103
+  }else if(species == "P. elliotti")
+  {
+    b5 = -3.538
+    b6 = 1.689
+    b7 = -2.237
+    b8 = 89.128
+    a1 = 0.694
+    a2 = 0.069
+  }else if(species == "P. patula")
+  {
+    b5 = -3.268
+    b6 = 1.502
+    b7 = -1.783
+    b8 = 23.772
+    a1 = 0.731
+    a2 = 0.094
+  }else if(species == "P. pinaster")
+  {
+    b5 = -2.66
+    b6 = 1.432
+    b7 = -1.572
+    b8 = 18.557
+    a1 = 0.766
+    a2 = 0.117
+  }else if(species == "P. radiata")
+  {
+    b5 = -3.283
+    b6 = 1.608
+    b7 = -1.287
+    b8 = 16.253
+    a1 = 0.737
+    a2 = 0.079
+  }else if(species == "P. taeda")
+  {
+    b5 = -4.378
+    b6 = 2.197
+    b7 = -2.651
+    b8 = 81.195
+    a1 = 0.721
+    a2 = 0.071
+  }
+
+  l1<-ifelse(h/H<= a1, 1, 0)
+  l2<-ifelse(h/H<= a2, 1, 0)
+
+  d2 = DBH^2 * (b5*((h/H) -1) + b6*((h/H)^2 -1) + (b7*(a1 - h/H)^2) * l1 + (b8*(a2 - h/H)^2) *l2)
+  d = sqrt(d2)
+
+  return(d)
+
+}
